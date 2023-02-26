9ce1b106cd97b3145a44b174ff938b271960bedc
GoGoder <125727645+GoGoder@users.noreply.github.com>
2023-02-27 00:04:04 +0800

    Add files via upload

---
+++ A1103324_0224_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class A1103324_0224_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("請輸入一個整數：");
+        int number = scanner.nextInt();
+
+        if (number % 2 == 0) {
+            System.out.println(number + " 是偶數。");
+        } else {
+            System.out.println(number + " 是奇數。");
+        }
+    }
+}
